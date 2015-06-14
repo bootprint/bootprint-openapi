1a557592fd8402ddc09579f5d0201d5b7308b167
Nils Knappmeier <n.knappmeier@i-views.de>
2015-06-14 08:49:47 +0200

    Correct type ("(" instead of ")")

--- handlebars/helpers.js
+++ handlebars/helpers.js
@@ -4,7 +4,7 @@ module.exports = {
             'csv': 'comma separated (`foo,bar`)',
             'ssv': 'space separated (`foo bar`)',
             'tsv': 'tab separated (`foo\\tbar`)',
-            'pipes': 'pipe separated )`foo|bar`)',
+            'pipes': 'pipe separated (`foo|bar`)',
             'multi': 'multiple parameters (`foo=bar&foo=baz`)'
         }[value];
     }
