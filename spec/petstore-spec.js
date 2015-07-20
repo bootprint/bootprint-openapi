8e01db4d68438f86c6ff580d0384ed1b9057c96b
Nils Knappmeier <n.knappmeier@i-views.de>
2015-07-20 23:37:38 +0200

    Test case for summary-link to paths. Add proper test-scripts

--- spec/petstore-spec.js
+++ spec/petstore-spec.js
@@ -12,28 +12,36 @@ var fs = require("fs");
 var cheerio = require("cheerio");
 
 describe("The petstore example", function () {
-    var $ = null;
-    beforeAll(function (done) {
-        require('bootprint')
-            .load(require('../'))
-            .build('spec/fixtures/petstore.json', 'test-output')
-            .generate()
-            .done(function () {
-                fs.readFile("test-output/index.html", {
-                    encoding: "utf-8"
-                }, function (err, data) {
-                    if (err) {
-                        done(err);
-                    }
-                    $ = cheerio.load(data);
-                    done();
-                })
-            });
+  var $ = null;
+  beforeAll(function (done) {
+    require('bootprint')
+      .load(require('../'))
+      .build('spec/fixtures/petstore.json', 'test-output')
+      .generate()
+      .done(function () {
+        fs.readFile("test-output/index.html", {
+          encoding: "utf-8"
+        }, function (err, data) {
+          if (err) {
+            done(err);
+          }
+          $ = cheerio.load(data);
+          done();
+        })
+      });
 
-    });
+  });
 
-    it("should contain 'Pet' as request body spec for /pet-POST", function () {
-        expect($("#operation--pet-post div.sw-request-model a.json-schema-ref").text())
-            .toBe("Pet");
-    })
+  it("should contain 'Pet' as request body spec for /pet-POST", function () {
+    expect($("#operation--pet-post div.sw-request-model a.json-schema-ref").text())
+      .toBe("Pet");
+  })
+
+  it("should contain an summary item '/pet' linking to '#path--pet'", function () {
+    expect($(".swagger--summary-path a[href='#path--pet']").text()).toBe("/pet");
+  });
+
+  it("should contain an path item '/pet' with id 'path--pet'", function () {
+    expect($("#path--pet").length).toBe(1);
+  });
 });
