19e8697e15c220eb292e0b54f36c8a040b771bc4
Nils Knappmeier <n.knappmeier@i-views.de>
2015-08-13 00:18:49 +0200

    Move from jasmine to mocha

--- test/petstore-spec.js
+++ test/petstore-spec.js
@@ -10,13 +10,14 @@
 /* global beforeAll */
 var fs = require('fs')
 var cheerio = require('cheerio')
+var expect = require("chai").expect;
 
 describe('The petstore example', function () {
   var $ = null
-  beforeAll(function (done) {
+  before(function (done) {
     require('bootprint')
       .load(require('../'))
-      .build('spec/fixtures/petstore.json', 'test-output')
+      .build('test/fixtures/petstore.json', 'test-output')
       .generate()
       .done(function () {
         fs.readFile('test-output/index.html', {
@@ -34,14 +35,14 @@ describe('The petstore example', function () {
 
   it("should contain 'Pet' as request body spec for /pet-POST", function () {
     expect($('#operation--pet-post div.sw-request-model a.json-schema-ref').text())
-      .toBe('Pet')
+      .to.equal('Pet')
   })
 
   it("should contain an summary item '/pet' linking to '#path--pet'", function () {
-    expect($(".swagger--summary-path a[href='#path--pet']").text()).toBe('/pet')
+    expect($(".swagger--summary-path a[href='#path--pet']").text()).to.equal('/pet')
   })
 
   it("should contain an path item '/pet' with id 'path--pet'", function () {
-    expect($('#path--pet').length).toBe(1)
+    expect($('#path--pet').length).to.equal(1)
   })
 })
