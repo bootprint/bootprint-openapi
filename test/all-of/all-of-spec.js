6ad928fd8a39aea94dbe30c4e51aa1c153f8102b
Nils Knappmeier <n.knappmeier@i-views.de>
2015-09-27 21:03:07 +0200

    bootprint-swagger#35: "allOf" renders incorrect HTML doc
    Fix in bootprint-json-schema, testcase in bootprint-swagger

---
+++ test/all-of/all-of-spec.js
@@ -0,0 +1,30 @@
+/*!
+ * bootprint-swagger <https://github.com/nknapp/bootprint-swagger>
+ *
+ * Copyright (c) 2015 Nils Knappmeier.
+ * Released under the MIT license.
+ */
+
+/* global describe */
+/* global it */
+/* global before */
+var expect = require('chai').expect
+var core = require('../core')
+
+describe('The all-of fixture', function () {
+  this.timeout(10000)
+  var context = {}
+  before(function () {
+    return core.run(require('./swagger.json'), __dirname, context)
+  })
+
+  it('should have an inreheritance block for definition AAA', function () {
+    expect(context.$('#definition-AAA').text())
+      .to.contain('BBB')
+  })
+
+  it('should have an inreheritance block for definition BBB', function () {
+    expect(context.$('#definition-BBB').text())
+      .to.contain('CCC')
+  })
+})
