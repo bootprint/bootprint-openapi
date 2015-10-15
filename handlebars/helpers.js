005521df559969a0afc5e59f1c0aadbfe5d2e896
Nils Knappmeier <n.knappmeier@i-views.de>
2015-10-15 21:10:56 +0200

    standard js format

--- handlebars/helpers.js
+++ handlebars/helpers.js
@@ -1,11 +1,11 @@
 module.exports = {
   'swagger--collection-format': function (value, paramName) {
     return {
-      'csv': 'comma separated (`'+paramName+'=aaa,bbb`)',
-      'ssv': 'space separated (`'+paramName+'=aaa bbb`)',
-      'tsv': 'tab separated (`'+paramName+'=aaa\\tbbb`)',
-      'pipes': 'pipe separated (`'+paramName+'=aaa|bbb`)',
-      'multi': 'multiple parameters (`'+paramName+'=aaa&'+paramName+'=bbb`)'
+      'csv': 'comma separated (`' + paramName + '=aaa,bbb`)',
+      'ssv': 'space separated (`' + paramName + '=aaa bbb`)',
+      'tsv': 'tab separated (`' + paramName + '=aaa\\tbbb`)',
+      'pipes': 'pipe separated (`' + paramName + '=aaa|bbb`)',
+      'multi': 'multiple parameters (`' + paramName + '=aaa&' + paramName + '=bbb`)'
     }[value]
   },
   'swagger--response-code': function (code) {
