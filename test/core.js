eaf87842eae0b299e1e2fc83abab36a11f38785c
Nils Knappmeier <github@knappi.org>
2016-11-17 22:25:33 +0100

    Create screenshots of each spec while running the tests in 400, 800 and 1200px width

--- test/core.js
+++ test/core.js
@@ -7,6 +7,15 @@
 var qfs = require('m-io/fs')
 var cheerio = require('cheerio')
 var path = require('path')
+var Nightmare = require('nightmare')
+var fileurl = require('file-url')
+var nightmare = Nightmare({show: false})
+
+process.on('exit', () => {
+  nightmare
+    .end()
+    .then(() => console.log('nightmare done'))
+})
 
 /**
  * Run bootprint with a fixture and return a cheerio wrapper for the index.html
@@ -20,6 +29,19 @@ function runBootprint (swaggerDefinition, dir, context) {
     .load(require('../'))
     .build(swaggerDefinition, targetDir)
     .generate()
+    .then(function () {
+      return nightmare
+        .goto(fileurl(path.join(targetDir, 'index.html')))
+        .viewport(400, 1200)
+        .wait(500)
+        .screenshot(path.join(targetDir, 'index-400.png'))
+        .viewport(800, 1200)
+        .wait(500)
+        .screenshot(path.join(targetDir, 'index-800.png'))
+        .viewport(1200, 1200)
+        .wait(500)
+        .screenshot(path.join(targetDir, 'index-1200.png'))
+    })
     .then(function () {
       return qfs.read(path.join(targetDir, 'index.html'))
     })
